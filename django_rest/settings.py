9590b3811822933f2f718327ce006f6fb00e3591
FelipeHardmann <fashardmann@gmail.com>
2023-06-27 18:13:55 -0300

    :wrench: add: new protected auth

--- django_rest/settings.py
+++ django_rest/settings.py
@@ -137,6 +137,9 @@ REST_FRAMEWORK = {
 
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'oauth2_provider.contrib.rest_framework.OAuth2Authentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
     )
 }
 
